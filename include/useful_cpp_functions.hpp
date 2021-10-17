8e30af39a350dfa064aead98a07f9e20b01ba0da
Armend Veseli <armend.veseli@yahoo.com>
2021-10-17 17:37:45 -0400

    Update useful_cpp_functions.hpp

--- include/useful_cpp_functions.hpp
+++ include/useful_cpp_functions.hpp
@@ -4,4 +4,6 @@ namespace useful_cpp_functions
 {
   void print(const std::string& message);
   void pause_console(const std::string& pause_message);
+  float get_fractioal_part(float f);
+  double get_fractioal_part(double d);
 };
